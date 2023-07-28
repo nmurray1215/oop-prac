d904d0ce4b90e55e102f40e18b16e49810db126b
Nathan Murray <namurray1215@gmail.com>
2023-07-28 14:53:31 -0500

    test2

--- index.js
+++ index.js
@@ -8,4 +8,14 @@ return {
 };
 };
 
-circle.draw();
+let circle = createCirlce(1);
+
+//constructor
+function Circle(radius){
+    this.radius = radius;
+    this.draw = function(){
+        console.log(draw);
+    }
+}
+
+let another = new Circle(1);
