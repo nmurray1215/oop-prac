85c58a7653866076dc8f4341a59067a04c3b10f9
Nathan Murray <namurray1215@gmail.com>
2023-07-28 15:46:18 -0500

    stopwatch exer

--- index.js
+++ index.js
@@ -1,16 +1,35 @@
-function Circle (radius){
-    this.radius = radius;
+function Stopwatch (){
+   
+let startTime, endTime, running, duration=0;
 
-    let defaultLocation = {x:0,y:0};
-    let computeOptimumLocation = function(factor) {
-        //.......
-    }
-    this.draw = function() {
-       computeOptimumLocation(0.1);
+this.start = function() {
+if(running)
+throw new Error('stopwatch is already running');
 
-    console.log('draw');
-    };
+running = true;
+startTime = new Date();
+};
+
+this.stop = function(){
+    if (!running)
+    throw new Error('stopwatch is not running');
+
+    running = false;
+    endTime = new Date();
+
+    let timeElapsed = (endTime.getTime() - startTime.getTime())/1000;
+    duration += timeElapsed;
+    
+};
+
+this.reset = function(){
+    startTime = null;
+    endTime = null;
+    running = false;
+    duration = 0;
+};
+Object.defineProperty(this,'duration', {
+    get: function() {return duration;}
+});
+
 }
-
-let circle = new Circle(10);
-circle.draw;
