fe96f6cf551590e2914b2bf44c22f5583a518213
Nathan Murray <namurray1215@gmail.com>
2023-07-28 14:44:53 -0500

    test

--- index.js
+++ index.js
@@ -1,13 +1,11 @@
-
-console.log("hello world");
-
-const circle ={
-    radius: 1,
-    location : {
-        x:1,
-        Y:2
-    },
-    draw: function () {
-        console.log('draw');
+//factory function
+function createCirlce(radius){
+return {
+    radius,
+    draw: function(){
+        console.log("draw");
     }
 };
+};
+
+circle.draw();
