bd2212025ac58651c595556085b8989bff0c2953
Nathan Murray <namurray1215@gmail.com>
2023-07-28 13:59:48 -0500

    initial commit

---
+++ index.js
@@ -0,0 +1,13 @@
+
+console.log("hello world");
+
+const circle ={
+    radius: 1,
+    location : {
+        x:1,
+        Y:2
+    },
+    draw: function () {
+        console.log('draw');
+    }
+};
